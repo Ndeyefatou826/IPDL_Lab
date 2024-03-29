134b3730ac7f349a089d1054062cfbffb5f7be2e
Ndeyefatou826 <ndeyendeyedabi@gmail.com>
2024-03-29 12:59:45 +0100

    LAB1
    LAB1

---
+++ sbCar/src/test/java/spark/batch/sbcar/SbCarApplicationTests.java
@@ -0,0 +1,13 @@
+package spark.batch.sbcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
